233a8195f0d28ced45af132a37d2fb5a3a96c833
ShayeGun <shayegan.7200@gmail.com>
2023-08-10 14:57:57 +0330

    environment file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }),
+  TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
-    port: 5434,
-    username: 'postgres',
-    password: '123',
-    database: 'firstNest',
+    host: process.env.DATABASE_URL,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
     entities: [User],
     synchronize: true,
-  }), AuthModule, UserModule],
+  }),
+    AuthModule,
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule { }
